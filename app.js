0824cded0dbf97b24f3675b3b4fbc791040ad2e7
Murat Çelik <celikmurat1845@gmail.com>
2022-07-21 17:03:00 +0300

    CREATE config file

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const userRouter = require('./routes/userRoutes')
 const app = express();
 
 // middleware's
+console.log(process.env.NODE_ENV)
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
-
-app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log("Hello from middleware 👋");
