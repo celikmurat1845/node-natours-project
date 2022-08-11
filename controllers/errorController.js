8946de60db2dffad3e636e03adf32821f10766d9
Murat Çelik <celikmurat1845@gmail.com>
2022-08-11 18:47:36 +0300

    CREATE models and schema

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,22 @@
+const AppError = require('./../utils/appError')
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`
+    return new AppError(message, 400)
+};
+
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]; 
+    const message = `Duplicate field value: ${value}. Please use another value!`;
+    return new AppError(message, 400);
+}
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+    const message = `Invalid input data. ${errors.join('. ')}`
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
     console.log("errStack:>>", err.stack);
     res.status(err.statusCode).json({ 
@@ -35,6 +54,16 @@ module.exports = (err, req, res, next) => {
     if(process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res)
     } else if(process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res)
+        let error = {...err};
+        // send message for db CastError
+        if(error.name === 'CastError') error = handleCastErrorDB(error);
+
+        // send message for db duplicate error
+        if(error.code === 11000) error = handleDuplicateFieldsDB(error);
+
+        // send message mongoose validation error
+        if(error.name === 'ValidationError') error = handleValidationErrorDB(error);
+
+        sendErrorProd(error, res)
     } 
 }
